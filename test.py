ad914ec641d0c676901cefb240549a6f225bd827
Matt Huisman <me@matthuisman.nz>
2018-07-16 15:30:29 +1200

    Update to work with renamed test

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import os
 import sys
 sys.path.append(os.path.realpath('../script.module.matthuisman/lib'))
 
-from matthuisman.tester import run_test
+from matthuisman.test import run_test
 from resources.lib.controller import Controller
 
 ROUTES = [
